118559ab6edf34d20c549a4cfc117c31d696691f
Jan Supolik <jan.supolik@hotmail.com>
2026-02-05 21:51:34 +0100

    https

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://127.0.0.1:8000/api'
-  //apiUrl: 'https://greyhill-api.azurewebsites.net/api'
-  apiUrl: 'http://greyhill-api.azurewebsites.net/api'
+  apiUrl: 'https://greyhill-api.azurewebsites.net/api'  
 };
