49dec000ea42c1652afa0d1a388147be296a7e9a
Jan Supolik <jan.supolik@hotmail.com>
2026-02-05 16:26:29 +0100

    2

--- greyhill-backend/app/main.py
+++ greyhill-backend/app/main.py
@@ -9,7 +9,7 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Greyhill API",
-    description="Booking systém pre apartmány 1",
+    description="Booking systém pre apartmány 2",
     version="1.0.0"
 )
 
