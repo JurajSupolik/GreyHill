3f1fadfa48966a6585c6f51d305b342d8360f7df
Jan Supolik <jan.supolik@hotmail.com>
2026-02-07 13:16:38 +0100

    --configuration test 3

--- client/src/app/components/test-api/test-api.component.ts
+++ client/src/app/components/test-api/test-api.component.ts
@@ -10,8 +10,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './test-api.component.html',
   styleUrls: ['./test-api.component.css']
 })
-export class TestApiComponent {
-  //url: string = '';
+export class TestApiComponent {  
   //url: string = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m';
   url: string = 'https://greyhill-api.azurewebsites.net/api/apartments/1';  
   response: string = '';
