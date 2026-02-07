3cf47d0cf21894216df528302cd094e16270c7e4
Jan Supolik <jan.supolik@hotmail.com>
2026-02-07 17:42:24 +0100

    307 redirect opravene

--- greyhill-backend/app/main.py
+++ greyhill-backend/app/main.py
@@ -21,7 +21,6 @@ console_logger = LoggerFactory.create_console_logger()
 file_logger = LoggerFactory.create_file_logger(file_path="logs/api_requests.log")
 
 app = FastAPI(
-    #redirect_slashes=False,
     title="Greyhill API",
     description="Booking systém pre apartmány Greyhill 1:06",
     version="1.0.0"
@@ -53,24 +52,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Trust Azure's proxy headers
+# bug nesiel ssl zaciatok
+# Confiuracia pre Azure
 app.add_middleware(
     TrustedHostMiddleware,
     allowed_hosts=["*"]    
 )
-
 # Handle forwarded headers properly
 from starlette.middleware.base import BaseHTTPMiddleware
-
 class ProxyHeadersMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        # Azure passes these headers
-        if "x-forwarded-proto" in request.headers:
+    async def dispatch(self, request, call_next):        
+        if "x-forwarded-proto" in request.headers: # Azure passes these headers
             request.scope["scheme"] = request.headers["x-forwarded-proto"]
         response = await call_next(request)
         return response
-
 app.add_middleware(ProxyHeadersMiddleware)
+# bug nesiel ssl koniec
 
 # Pridanie vlastného middleware pre logovanie
 app.add_middleware(LoggingMiddleware,log_handler=custom_log_handler)
