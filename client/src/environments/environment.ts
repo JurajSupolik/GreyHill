c432e415b7b90b2aa9bbe3073818c7bd6f370eba
Jan Supolik <jan.supolik@hotmail.com>
2026-02-08 09:11:36 +0100

    test env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,  
-//apiUrl: 'https://greyhill-api.azurewebsites.net/api'
-  apiUrl: 'https://greyhill-test-api.azurewebsites.net/api'
+  apiUrl: 'https://greyhill-api.azurewebsites.net/api'
+  //apiUrl: 'https://greyhill-test-api.azurewebsites.net/api'
 };
